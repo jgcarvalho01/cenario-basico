a65a1f14b05115bc0e9f4f76b5b043c3fc183f6e
João Gabriel Carvalho <41981581+jgcarvalho01@users.noreply.github.com>
2023-04-21 23:53:26 -0300

    criando um elemento WebGLRenderer

--- script.js
+++ script.js
@@ -17,3 +17,13 @@ const sizes = {
 
 const camera = new THREE.PerspectiveCamera(75,sizes.width/sizes.height)
 scene.add(camera)
+
+const canvas = document.querySelector('.webgl')
+
+const renderer = new THREE.WebGLRenderer({
+	canvas: canvas
+})
+
+renderer.setSize(sizes.width, sizes.height)
+
+renderer.render(scene, camera)
